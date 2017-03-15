ce65f4ec347b182eb9f868b727a8377ec89807c9
johana tangarife <johana tangarife>
2017-03-14 20:31:29 -0500

    se crea el contador

--- Numprimos.java
+++ Numprimos.java
@@ -16,7 +16,21 @@ public class Numprimos {
      */
     public static void main(String[] args) {
         
-       
+         int count = 0;
+
+            for (int i = 2; i <= 100; i++)
+            {
+
+                for (int x = 1; x <= i; x++)
+                {
+                    if (i % x == 0)
+                    {
+                        count = count + 1;
+
+                    
+                
+              
+            }
     }
     
 }
